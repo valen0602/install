348ba9245ecdbba29d145d80c25e48311d49cbbf
Valen0602 <45152685+valen0602@users.noreply.github.com>
2019-08-01 06:22:37 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,50 @@
+clear
+
+figlet valen0602 | lolcat
+
+echo "Di Sarankan Untuk menggunakan Wifi Saat Menginstall" | lolcat
+
+echo "tunggu sebentar file akan di install" | lolcat
+
+echo "...GO..." | lolcat
+pkg update
+
+pkg upgrade
+
+pkg install git
+
+pkg install php
+
+pkg install python
+
+pkg install python2
+
+pkg install python3
+
+pkg install lolcat
+
+pkg install nano
+
+pkg install ruby
+
+pkg install toilet
+
+pkg install mc
+
+pkg install cowsay
+
+pkg install figlet
+
+echo "Terima kasih Telah Menggunakan Script Valen0602" | lolcat
+
+echo "pembersih otomatis" | lolcat
+
+echo "---3---" | lolcat
+
+echo "---2---" | lolcat
+
+echo "---1---" | lolcat
+
+echo "By...." | lolcat
+
+clear
